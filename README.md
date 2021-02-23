64e6761ca302beee7bd0aa6e5cb882671b2852ac
sal sandoval <myemailaddress>
2021-02-22 22:08:57 -0700

    finished project

--- README.md
+++ README.md
@@ -43,7 +43,12 @@ Find the file `index.js` and complete the tasks.
 Edit the `ReadMe` file with your answers.
 
 1. In your own words, define closure (1-2 sentences).
+
+ a closure gives you acces to a functions scope
+
 2. Study the following code, then answer the questions below.
+
+
 
 ```js
 function personalDice(name){
@@ -64,7 +69,13 @@ dansRoll();
 ```
 
 a. Where is closure used in this code? How can you tell?
+
+
+
 b. Compare and contrast calling `dansRoll` the first and second time. What is always the same? What could change?
+
+
+
 c. What is the lexical scope of `newRoll`? 
 
 
--- index.js
+++ index.js
@@ -2,24 +2,27 @@
 
 /**Example Task : processFirstItem()
  * This example shows how you might go about solving the rest of the tasks
- * 
+ *
  * Use the higher order function processFirstItem below to do the following:
  *  1. Receive an array of strings in a parameter
  *  2. Receive a callback function that takes a string as its argument in a parameter
- *  3. Return the result of invoking the callback function and passing in the FIRST 
+ *  3. Return the result of invoking the callback function and passing in the FIRST
  *     element in the array as the argument
- * 
+ *
  * The following code is demonstrating a way of completing this task
  * It returns the string `foofoo`
-*/
+ */
 
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
-console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
+console.log(
+  processFirstItem(["foo", "bar"], function (str) {
+    return str + str;
+  })
+);
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
@@ -45,8 +48,8 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -58,7 +61,6 @@ function counter2() {
   return count++;
 }
 
-
 /* ⚾️⚾️⚾️ Task 2: inning() ⚾️⚾️⚾️
 Use the inning function below to do the following:
   1. Return a random whole number of points between 0 and 2 scored by one team in an inning
@@ -68,15 +70,10 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(){
-    return math.floor(Math.random() * (3));
+function inning() {
+  return Math.floor(Math.random() * 3);
 }
 console.log(inning);
-
-
-
-
-
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -90,32 +87,35 @@ Use the finalScore function below to do the following:
   "Home": 11,
   "Away": 5
 }
-*/ 
+*/
 
-function finalScore(func, innings){
- let Home = 0;
- let Away = 0;
-for(let i = 0; i <= innings; i++){
-  Home = Home +func;
-  Away = Away + func;
- return home 
+function finalScore(func, innings) {
+  let home = 0;
+  let away = 0;
+  for (let i = 0; i < innings; i++) {
+    home = home + func();
+    away = away + func();
+  }
+  return {
+    Home: home,
+    Away: away,
+  };
 }
-
-}
-console.log(finalScore());
-
-
-
+console.log(finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
+function getInningScore(func) {
   /*Your Code Here */
+  return {
+    Home: func(),
+    Away: func(),
+  };
 }
-
+console.log(getInningScore(inning));
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
@@ -158,19 +158,41 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningScoreFunc, inningFunc, innings) {
+  let scores = [];
+  for (let i = 0; i < innings; i++) {
+    scores.push(getInningScoreFunc(inningFunc));
+  }
+  let homeTotal = 0;
+  let awayTotal = 0;
+  let stringArray = [];
+  for (let i = 0; i < scores.length; i++) {
+    stringArray.push(
+      "Inning " +
+        (i + 1) +
+        ": Away " +
+        scores[i].Away +
+        " - Home " +
+        scores[i].Home
+    );
+    homeTotal = homeTotal + scores[i].Home;
+    awayTotal = awayTotal + scores[i].Away;
+  }
+ if(homeTotal == awayTotal){
+   stringArray.push("This game will require extra innings: Away " + awayTotal + " - Home " + homeTotal);
+ } else {
+   stringArray.push ( "Final Score: Away " + awayTotal + " - Home " + homeTotal);
+ }
+ 
+  return stringArray;
 }
-
-
-
-
+console.log(scoreboard(getInningScore, inning, 9));
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
-function foo(){
+function foo() {
   //console.log('its working');
-  return 'bar';
+  return "bar";
 }
-export default{
+export default {
   foo,
   processFirstItem,
   counter1,
@@ -179,4 +201,4 @@ export default{
   finalScore,
   getInningScore,
   scoreboard,
-}
+};
